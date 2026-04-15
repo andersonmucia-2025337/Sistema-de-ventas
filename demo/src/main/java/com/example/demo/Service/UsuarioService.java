a8e7b90d54cde02be3a47249a02c85e6d09966f9
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-04-15 01:16:52 -0600

    feat(Service): Agregar metodo buscarPorUsername en UsuarioService

--- demo/src/main/java/com/example/demo/Service/UsuarioService.java
+++ demo/src/main/java/com/example/demo/Service/UsuarioService.java
@@ -6,6 +6,7 @@ import java.util.List;
 public interface UsuarioService {
     List<Usuario> listarUsuarios();
     Usuario buscarUsuarioPorId(Integer codigoUsuario);
+    Usuario buscarPorUsername(String username);
     Usuario guardarUsuario(Usuario usuario) throws RuntimeException;
     Usuario actualizarUsuario(Integer codigoUsuario, Usuario usuario) throws RuntimeException;
     void eliminarUsuario(Integer codigoUsuario) throws RuntimeException;
