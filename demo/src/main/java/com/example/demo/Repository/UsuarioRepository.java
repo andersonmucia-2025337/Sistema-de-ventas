a26279573b18ba28a0f66740c8687d4d3c10a885
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-04-15 01:16:34 -0600

    feat: Agregar metodo findByUsername en UsuarioRepository

--- demo/src/main/java/com/example/demo/Repository/UsuarioRepository.java
+++ demo/src/main/java/com/example/demo/Repository/UsuarioRepository.java
@@ -3,8 +3,9 @@ package com.example.demo.Repository;
 import com.example.demo.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+    Optional<Usuario> findByUsername(String username);
 }
